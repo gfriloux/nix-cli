240af59161b5007b2f6582f98f63c3e4452d21f2
Guillaume Friloux <gfriloux@datasolution.fr>
2025-10-21 17:59:00 +0200

    feat(all): Install atuin if module enabled
    Install atuin if module enabled
    
    Signed-off-by: Guillaume Friloux

--- modules/home-manager/atuin.nix
+++ modules/home-manager/atuin.nix
@@ -1,19 +1,10 @@
 { lib, config, ... }:
 
 let
-  cfg = config.nix-cli.hm.git;
+  cfg = config.nix-cli.hm;
 in
 {
-  options.nix-cli.hm.git = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = config.nix-cli.hm.enable;
-      description = "Enable ATUIN module";
-    };
-  };
-
   config = lib.mkIf cfg.enable {
-
     programs.atuin = {
       enable = true;
       #enableFishIntegration = true;
